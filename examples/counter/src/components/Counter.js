cbf06b174500b48a082c36d173a022f708a8b516
郝海亮 <2298763729@qq.com>
2018-07-06 15:51:54 +0800

    format:搭建环境的计数器

--- examples/counter/src/components/Counter.js
+++ examples/counter/src/components/Counter.js
@@ -1,53 +1,50 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import PropTypes            from 'prop-types'
 
 class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.incrementAsync = this.incrementAsync.bind(this);
-    this.incrementIfOdd = this.incrementIfOdd.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		// 重新绑定this指针
+		this.incrementAsync = this.incrementAsync.bind(this);
+		this.incrementIfOdd = this.incrementIfOdd.bind(this);
+	}
 
-  incrementIfOdd() {
-    if (this.props.value % 2 !== 0) {
-      this.props.onIncrement()
-    }
-  }
+	incrementIfOdd() {
+		if (this.props.value % 2 !== 0) {
+			// 回调父组件的方法
+			this.props.onIncrement()
+		}
+	}
 
-  incrementAsync() {
-    setTimeout(this.props.onIncrement, 1000)
-  }
+	incrementAsync() {
+        // 回调父组件的方法
+		setTimeout(this.props.onIncrement, 3000)
+	}
 
-  render() {
-    const { value, onIncrement, onDecrement } = this.props
-    return (
-      <p>
-        Clicked: {value} times
-        {' '}
-        <button onClick={onIncrement}>
-          +
-        </button>
-        {' '}
-        <button onClick={onDecrement}>
-          -
-        </button>
-        {' '}
-        <button onClick={this.incrementIfOdd}>
-          Increment if odd
-        </button>
-        {' '}
-        <button onClick={this.incrementAsync}>
-          Increment async
-        </button>
-      </p>
-    )
-  }
+	render() {
+		// 对应它的3个属性
+		const { value, onIncrement, onDecrement } = this.props
+		return (
+			<p>
+				Clicked: {value} times
+				{' '}
+				<button onClick={onIncrement}> + </button>{/* 回调父组件的方法 */}
+				{' '}
+				<button onClick={onDecrement}> - </button>{/* 回调父组件的方法 */}
+				{' '}
+				<button onClick={this.incrementIfOdd}> Increment if odd </button>{/* 调用自身的方法 */}
+				{' '}
+				<button onClick={this.incrementAsync}> Increment async </button>{/* 调用自身的方法 */}
+			</p>
+		)
+	}
 }
 
+// 属性限制
 Counter.propTypes = {
-  value: PropTypes.number.isRequired,
-  onIncrement: PropTypes.func.isRequired,
-  onDecrement: PropTypes.func.isRequired
+	value       : PropTypes.number.isRequired,
+	onIncrement : PropTypes.func.isRequired,
+	onDecrement : PropTypes.func.isRequired
 }
 
 export default Counter
