90acc0346cb61d273beb1cf8a583a0ab8df7f764
郝海亮 <2298763729@qq.com>
2018-07-07 10:49:28 +0800

    footer逻辑疏通

--- examples/todos/src/components/Link.js
+++ examples/todos/src/components/Link.js
@@ -1,22 +1,23 @@
 import React     from 'react'
 import PropTypes from 'prop-types'
 
+// 属性作为组件的参数传递过来
 const Link = ({ active, children, onClick }) => (
-    <button
-       onClick={onClick}
-       disabled={active}
-       style={{
-           marginLeft: '4px',
-       }}
-    >
-      {children}
-    </button>
+	<button
+		onClick={onClick}
+		disabled={active}
+		style={{
+			marginLeft: '4px',
+		}}
+	>
+		{children}
+	</button>
 )
 
 Link.propTypes = {
-  active: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
-  onClick: PropTypes.func.isRequired
+	active   : PropTypes.bool.isRequired,
+	children : PropTypes.node.isRequired,
+	onClick  : PropTypes.func.isRequired
 }
 
 export default Link
--- examples/todos/src/containers/FilterLink.js
+++ examples/todos/src/containers/FilterLink.js
@@ -2,15 +2,24 @@ import { connect }             from 'react-redux'
 import { setVisibilityFilter } from '../actions'
 import Link                    from '../components/Link'
 
-const mapStateToProps = (state, ownProps) => ({
-  active: ownProps.filter === state.visibilityFilter
-})
+// const mapStateToProps = (state, ownProps) => ({
+//   active: ownProps.filter === state.visibilityFilter
+// })
+
+// ownProps的属性从哪里获得的呢?
+// 从父组件中获取的 {filter: "SHOW_ALL", children: "All"}
+const mapStateToProps = (state, ownProps) => {
+	console.log( state, ownProps );
+	return {
+		active: ownProps.filter === state.visibilityFilter
+	}
+}
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
+	onClick: () => dispatch(setVisibilityFilter(ownProps.filter))
 })
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(Link)
