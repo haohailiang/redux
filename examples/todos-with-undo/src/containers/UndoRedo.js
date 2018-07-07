db3e7bc31bcb26056c591d401f2026f3f576b0ef
郝海亮 <2298763729@qq.com>
2018-07-07 12:16:42 +0800

    redo/undo没看懂

--- examples/todos-with-undo/src/containers/UndoRedo.js
+++ examples/todos-with-undo/src/containers/UndoRedo.js
@@ -1,31 +1,27 @@
-import React from 'react'
+import React                                    from 'react'
 import { ActionCreators as UndoActionCreators } from 'redux-undo'
-import { connect } from 'react-redux'
+import { connect }                              from 'react-redux'
 
 let UndoRedo = ({ canUndo, canRedo, onUndo, onRedo }) => (
-  <p>
-    <button onClick={onUndo} disabled={!canUndo}>
-      Undo
-    </button>
-    <button onClick={onRedo} disabled={!canRedo}>
-      Redo
-    </button>
-  </p>
+	<p>
+		<button onClick={onUndo} disabled={!canUndo}> Undo </button>
+		<button onClick={onRedo} disabled={!canRedo}> Redo </button>
+	</p>
 )
 
 const mapStateToProps = (state) => ({
-  canUndo: state.todos.past.length > 0,
-  canRedo: state.todos.future.length > 0
+	canUndo: state.todos.past.length > 0,
+	canRedo: state.todos.future.length > 0
 })
 
 const mapDispatchToProps = ({
-  onUndo: UndoActionCreators.undo,
-  onRedo: UndoActionCreators.redo
+	onUndo: UndoActionCreators.undo,
+	onRedo: UndoActionCreators.redo
 })
 
 UndoRedo = connect(
-  mapStateToProps,
-  mapDispatchToProps
+	mapStateToProps,
+	mapDispatchToProps
 )(UndoRedo)
 
 export default UndoRedo
